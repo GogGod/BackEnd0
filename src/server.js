799ef5b8d195fe8f58435b834b260a8f65c0d940
GogGod <chauquoclamphong@gmail.com>
2024-08-07 00:15:36 +0700

    learning

---
+++ src/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const path = require('path')
+
+const port = process.env.PORT
+const hostname = process.env.HOST_NAME
+
+app.get('/', function (req, res) {
+  res.render('sample.ejs')
+})
+app.get('/abc', function (req, res) {
+  res.send('123')
+})
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.listen(port, hostname)
