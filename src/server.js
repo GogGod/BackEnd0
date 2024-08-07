40d2e022e4b8eedde8d99fd07cf801841f095e28
GogGod <chauquoclamphong@gmail.com>
2024-08-07 19:58:10 +0700

    update

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-require('dotenv').config()
 const path = require('path')
+require('dotenv').config()
 
 const port = process.env.PORT || 8080
 const hostname = process.env.HOST_NAME
