c0e569203fa72a10b9fe5dab3743591281c49d58
GogGod <chauquoclamphong@gmail.com>
2024-08-07 22:12:19 +0700

    update

--- src/server.js
+++ src/server.js
@@ -3,15 +3,23 @@ const app = express()
 const path = require('path')
 require('dotenv').config()
 
+//config static files
+app.use(express.static('public'))
+
+//hosting
 const port = process.env.PORT || 8080
 const hostname = process.env.HOST_NAME
 
+//config template engine
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+//khai báo route
 app.get('/', function (req, res) {
   res.render('sample.ejs')
 })
 app.get('/abc', function (req, res) {
-  res.send('123')
+  res.send('234')
 })
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+
 app.listen(port, hostname)
